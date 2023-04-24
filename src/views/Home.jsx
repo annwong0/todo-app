f6a3cd216615ba18b6331546fbdc1a9252154866
alicepywong <alicepywong715@gmail.com>
2023-04-25 02:01:58 +0800

    update files

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,26 +1,65 @@
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AllPanel from '../components/AllPanel';
 import ActivePanel from '../components/ActivePanel';
 import CompletedPanel from '../components/CompletedPanel';
 import Footer from '../components/Footer';
 import '../css/Home.css';
 import { Grid } from '@mui/material';
+import moment from 'moment/moment';
 
 function Home() {
     const [value, setValue] = useState(0);
-    const [todo, setTodo] = useState([]);
+    const [todoList, setTodoList] = useState([]);
+    const [addingTodo, setAddingTodo] = useState(false);
 
-    const handleChange = (event, newValue) => {
+    useEffect(() => {
+        readTodoList();
+    }, [])
+
+    const handleTabChange = (event, newValue) => {
         setValue(newValue);
-        console.log(value)
-    };
-
-    const handleAddTodo = (newTodo) => {
-        setTodo(newTodo, ...todo)
     }
+    /**
+     * read todo list
+     */
+    const readTodoList = () => {
+        let curTodoList = JSON.parse(localStorage.getItem('todo'));
+        if (!curTodoList) {
+            localStorage.setItem('todo', JSON.stringify([]));
+        }
+        setTodoList(curTodoList ? curTodoList : []);
+    }
+    /**
+     * create todo by reading text
+     * @param {String} text 
+     */
+    const createTodo = (text) => {
+        setAddingTodo(true);
+        const todoObj = {
+            id: moment().format(), 
+            text: text, 
+            active: true
+        }
+        let oldTodoList = JSON.parse(localStorage.getItem('todo'));
+        localStorage.setItem('todo', JSON.stringify(oldTodoList.concat(todoObj)))
+        setAddingTodo(false);
+        readTodoList();
+    }
+    /**
+     * update a todo
+     * @param {*} event 
+     * @param {String} id 
+     */
+    const updateTodo = (event, id) => {
+
+    }
+
+    useEffect(() => {
+        console.log(todoList)
+    }, [todoList])
 
     return (
         <div>
@@ -29,16 +68,16 @@ function Home() {
                 <Grid item xs={12} md={6}>
                     <div className='header'>#todo</div>
                     <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                        <Tabs value={value} onChange={handleChange} centered>
+                        <Tabs value={value} onChange={handleTabChange} centered>
                             <Tab label="All" id='tab-0' />
                             <Tab label="Active" id='tab-1'/>
                             <Tab label="Completed" id='tab-2'/>
                         </Tabs>
                         <div id='tab-0' hidden={value !== 0} className='content'>
-                            <AllPanel todo={todo} handleAddTodo={handleAddTodo}/>
+                            <AllPanel todoList={todoList} createTodo={createTodo} updateTodo={updateTodo}/>
                         </div>
                         <div id='tab-1' hidden={value !== 1} className='content'>
-                            <ActivePanel />
+                            <ActivePanel todoList={todoList.filter(todo => todo.active === true)} updateTodo={updateTodo}/>
                         </div>
                         <div id='tab-2' hidden={value !== 2} className='content'>
                             <CompletedPanel />
